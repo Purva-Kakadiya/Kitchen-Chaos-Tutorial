7258ccbc622665f678d60093b808505e2a60f7b4
Purva Kakadiya <tpurv08@gmail.com>
2026-06-21 23:04:31 +0530

    DeliveryManager with UI
    made DeliveryManager with DeliveryManagerUI

---
+++ Assets/Scripting/DeliveryManager.cs
@@ -0,0 +1,78 @@
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeliveryManager : MonoBehaviour {
+    public event EventHandler OnRecipeSpawn;
+    public event EventHandler OnRecipeComplete;
+
+    public static DeliveryManager Instance { get; private set; }
+
+    [SerializeField] private RecipeListSO recipeListSO;
+
+    private List<RecipeSO> waitingRecipeSOList;
+    private float spawnRecipeTimer;
+    private float spawnRecipeTimerMax = 4f;
+    private int waitingRecipesMax = 5;
+
+    private void Awake() {
+        Instance = this;
+
+        waitingRecipeSOList = new List<RecipeSO>();
+    }
+
+    private void Update() {
+        spawnRecipeTimer -= Time.deltaTime;
+        if(spawnRecipeTimer < 0f) {
+            spawnRecipeTimer = spawnRecipeTimerMax;
+
+            if(waitingRecipeSOList.Count < waitingRecipesMax) {
+                RecipeSO waitingRecipe = recipeListSO.recipeSOList[UnityEngine.Random.Range(0, recipeListSO.recipeSOList.Count)];
+           
+                waitingRecipeSOList.Add(waitingRecipe);
+
+                OnRecipeSpawn?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+
+    public void DeliverRecipe(PlateKitchenObject plateKitchenObject) {
+        for(int i = 0; i < waitingRecipeSOList.Count; i++) {
+            RecipeSO waitingRecipeSO = waitingRecipeSOList[i];
+
+            if(waitingRecipeSO.kitchenObjectSOList.Count == plateKitchenObject.GetKitchenObjectSOList().Count) {
+                bool plateContentMatchesRecipe = true;
+
+                foreach(KitchenObjectSO recipeKitchenObjectSO in waitingRecipeSO.kitchenObjectSOList) {
+                    bool ingredientFound = false;
+
+                    foreach(KitchenObjectSO plateKitchenObjectSO in plateKitchenObject.GetKitchenObjectSOList()) {
+                        if(plateKitchenObjectSO == recipeKitchenObjectSO) {
+                            ingredientFound = true;
+                            break;
+                        }
+                    }
+
+                    if(!ingredientFound) {
+                        plateContentMatchesRecipe = false;
+                    }
+                }
+
+                if(plateContentMatchesRecipe) {
+                    waitingRecipeSOList.RemoveAt(i);
+
+                    OnRecipeComplete?.Invoke(this, EventArgs.Empty);
+                    return;
+                }
+            }
+        }
+
+        Debug.Log("Player Delivered Wrong Recipe!");
+    }
+
+    public List<RecipeSO> GetWaitingRecipeSOList() {
+        return waitingRecipeSOList;
+    }
+
+}
---
+++ Assets/Scripting/ScriptableObject/RecipeSO.cs
@@ -0,0 +1,10 @@
+using NUnit.Framework;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "RecipeSO", menuName = "Scriptable Objects/RecipeSO")]
+public class RecipeSO : ScriptableObject {
+
+    public List<KitchenObjectSO> kitchenObjectSOList;
+    public string recipeName;
+}
